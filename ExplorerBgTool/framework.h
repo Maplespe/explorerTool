5f85972ca302e26a79cb5fa5c7736e2cce96ef6f
Maplespe <43676879+Maplespe@users.noreply.github.com>
2022-01-31 17:06:59 +0800

    Delete ExplorerBgTool directory

--- ExplorerBgTool/framework.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
-// Windows 头文件
-#include <windows.h>
-
-// 全局模块句柄 Global module handle
-extern HMODULE g_hModule;
--- ExplorerBgTool/minihook/hde/pch.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-#include "../pch.h"
